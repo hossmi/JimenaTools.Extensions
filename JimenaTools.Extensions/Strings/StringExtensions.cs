22ddadd19f087265c034471b80007c615a1f4b0e
Pablo Jimena <hossmi@gmail.com>
2023-02-26 21:50:37 +0100

    Add string extensions

---
+++ JimenaTools.Extensions/Strings/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using JimenaTools.Extensions.Validations;
+
+namespace JimenaTools.Extensions.Strings
+{
+    public static class StringExtensions
+    {
+        public static bool IsFilled(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        public static string Format(this string text, params object[] args)
+        {
+            return string.Format(text, args);
+        }
+
+        public static string AsJoined(this IEnumerable<string> chunks)
+        {
+            return string.Join(string.Empty, chunks);
+        }
+
+        public static string AsJoinedWith(this IEnumerable<string> chunks, string separator)
+        {
+            separator.ShouldBeFilled(nameof(separator));
+            return string.Join(separator, chunks);
+        }
+    }
+}
