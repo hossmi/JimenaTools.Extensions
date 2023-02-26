5bfa3f5a058e20c5daf74134c40a9f2a08c9d555
Pablo Jimena <hossmi@gmail.com>
2023-02-26 21:52:01 +0100

    Add validation extensions

---
+++ JimenaTools.Extensions/Validations/ShouldBeSingleValidationExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JimenaTools.Extensions.Validations
+{
+    public static class ShouldBeSingleValidationExtensions
+    {
+        public static T ShouldBeSingle<T>(this IEnumerable<T> items, Func<Exception> getException)
+        {
+            T[] result;
+
+            result = items
+                .ShouldBeNotNull(nameof(items))
+                .Take(2)
+                .ToArray();
+
+            if (result.Length == 1)
+                return result[0];
+
+            throw getException();
+        }
+
+        public static T ShouldBeSingle<T>(this IEnumerable<T> items, Func<IEnumerable<T>, Exception> getException)
+        {
+            T[] result;
+
+            result = items
+                .ShouldBeNotNull(nameof(items))
+                .Take(2)
+                .ToArray();
+
+            if (result.Length == 1)
+                return result[0];
+
+            throw getException(items);
+        }
+
+        public static T ShouldBeSingleOrDefault<T>(this IEnumerable<T> items, Func<Exception> getException)
+        {
+            T[] result;
+
+            result = items
+                .ShouldBeNotNull(nameof(items))
+                .Take(2)
+                .ToArray();
+
+            if (result.Length == 0)
+                return default;
+
+            if (result.Length == 1)
+                return result[0];
+
+            throw getException();
+        }
+
+        public static T ShouldBeSingleOrDefault<T>(this IEnumerable<T> items, Func<IEnumerable<T>, Exception> getException)
+        {
+            T[] result;
+
+            result = items
+                .ShouldBeNotNull(nameof(items))
+                .Take(2)
+                .ToArray();
+
+            if (result.Length == 0)
+                return default;
+
+            if (result.Length == 1)
+                return result[0];
+
+            throw getException(items);
+        }
+    }
+}
---
+++ JimenaTools.Extensions/Validations/ValidationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using JimenaTools.Extensions.Exceptions;
+
+namespace JimenaTools.Extensions.Validations
+{
+    public static class ValidationExtensions
+    {
+        public static T ShouldBeNotNull<T>(this T item, string paramName = null)
+            where T : class
+        {
+            if (item == null)
+                throw paramName != null
+                    ? new ArgumentNullException(paramName)
+                    : new ArgumentNullException();
+
+            return item;
+        }
+
+        public static string ShouldBeFilled(this string text, string paramName = null)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new RequiredStringArgumentException(paramName);
+
+            return text;
+        }
+
+        public static T ShouldBeNonDefaultValue<T>(this T value, string paramName = null)
+            where T : struct, IComparable<T>
+        {
+            if (value.CompareTo(default) == 0)
+                throw new NonDefaultValueArgumentException<T>(paramName);
+
+            return value;
+        }
+    }
+}
