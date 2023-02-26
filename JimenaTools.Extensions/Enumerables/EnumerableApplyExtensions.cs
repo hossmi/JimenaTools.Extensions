b0bead82bfd15a454b785bff233740c08f7063d8
Pablo Jimena <hossmi@gmail.com>
2023-02-26 21:51:43 +0100

    Add enumerables extensions

---
+++ JimenaTools.Extensions/Enumerables/EnumerableApplyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JimenaTools.Extensions.Enumerables
+{
+    public static class EnumerableApplyExtensions
+    {
+        public static IEnumerable<T> Apply<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (T item in items)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+
+        public static void AndApply<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (T item in items)
+                action(item);
+        }
+
+        public static void AndApply<T, TIgnored>(this IEnumerable<T> items, Func<T, TIgnored> action)
+        {
+            foreach (T item in items)
+                action(item);
+        }
+    }
+}
---
+++ JimenaTools.Extensions/Enumerables/EnumerableSetExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JimenaTools.Extensions.Enumerables
+{
+    public static class EnumerableSetExtensions
+    {
+        public static bool IsAnyOf<T>(this T item, params T[] values)
+        {
+            return values.Contains(item);
+        }
+
+        public static bool IsAnyOf<T>(this T item, IEnumerable<T> values)
+        {
+            return values.Contains(item);
+        }
+
+        public static HashSet<T> Substract<T>(this IEnumerable<T> items, IEnumerable<T> itemsToRemove)
+        {
+            HashSet<T> result;
+
+            result = items.ToHashSet();
+            result.ExceptWith(itemsToRemove);
+
+            return result;
+        }
+    }
+}
