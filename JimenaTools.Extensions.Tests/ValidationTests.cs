28eccfb9eed568415b55f0891195c859615e3de2
Pablo Jimena <hossmi@gmail.com>
2023-03-22 18:28:11 +0100

    Add ValidationTests

---
+++ JimenaTools.Extensions.Tests/ValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using JimenaTools.Extensions.Strings;
+
+namespace JimenaTools.Extensions.Tests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void AsJoined_allows_NewLine_separator()
+        {
+            string response;
+            string[] words;
+
+            words = new[]
+            {
+                "One",
+                "two",
+                "three",
+            };
+
+            response = words.AsJoinedWith(Environment.NewLine);
+
+            response.Should().Be(@"One
+two
+three");
+
+        }
+    }
+}
