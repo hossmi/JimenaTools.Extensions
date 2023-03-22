7a8426afb93db3f802a431917d806b0f28f61e8d
Pablo Jimena <hossmi@gmail.com>
2023-03-22 18:30:05 +0100

    Add documental comments

--- JimenaTools.Extensions/Exceptions/RequiredStringArgumentException.cs
+++ JimenaTools.Extensions/Exceptions/RequiredStringArgumentException.cs
@@ -3,14 +3,28 @@ using System.Runtime.Serialization;
 
 namespace JimenaTools.Exceptions
 {
+    /// <summary>
+    /// Exception thrown when a string parameter is null, empty or white space
+    /// </summary>
+    [Serializable]
     public class RequiredStringArgumentException : ArgumentException
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public RequiredStringArgumentException()
             : base($"{nameof(String)} parameter cannot be null or empty.") { }
 
+        /// <summary>
+        /// Constructor for named parameters.
+        /// </summary>
+        /// <param name="paramName">Name of the parameter.</param>
         public RequiredStringArgumentException(string paramName)
             : base($"{nameof(String)} parameter '{paramName}' cannot be empty.") { }
 
+        /// <summary>
+        /// Special constructor
+        /// </summary>
         protected RequiredStringArgumentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- JimenaTools.Extensions/Extensions/Strings/StringExtensions.cs
+++ JimenaTools.Extensions/Extensions/Strings/StringExtensions.cs
@@ -3,23 +3,38 @@ using JimenaTools.Extensions.Validations;
 
 namespace JimenaTools.Extensions.Strings
 {
+    /// <summary>
+    /// Set of <see cref="System.String"/> extension methods. 
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Returns the opposite to <see cref="System.String.IsNullOrWhiteSpace"/>.
+        /// </summary>
         public static bool IsFilled(this string value)
         {
             return !string.IsNullOrWhiteSpace(value);
         }
 
+        /// <summary>
+        /// Extension method for fluently call string.Format static method.
+        /// </summary>
         public static string Format(this string text, params object[] args)
         {
             return string.Format(text, args);
         }
 
+        /// <summary>
+        /// Extension method for fluently call string.Join static method.
+        /// </summary>
         public static string AsJoined(this IEnumerable<string> chunks)
         {
             return string.Join(string.Empty, chunks);
         }
 
+        /// <summary>
+        /// Extension method for fluently call string.Join static method.
+        /// </summary>
         public static string AsJoinedWith(this IEnumerable<string> chunks, string separator)
         {
             separator.ShouldBeNotNull(nameof(separator));
